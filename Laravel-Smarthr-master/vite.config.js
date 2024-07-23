f1919db088bac583dcb6fb3cc82e24d5a9695a9a
ICT-R433 <71366471+purevjav-ict@users.noreply.github.com>
2024-07-23 15:17:32 +0800

    Update vite.config.js

--- Laravel-Smarthr-master/vite.config.js
+++ Laravel-Smarthr-master/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
              "outputs": ["dist/**"]
-            input: [
+            "input": [
                 'resources/css/app.css', 
                 'resources/js/app.js'
             ],
